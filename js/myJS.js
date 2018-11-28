527cd559ab3ad9260a3b4f0a17e3239a5f8bcc04
Judy Nyibule <judynyibule@gmail.com>
2018-11-28 12:52:54 +0300

    Edit function to js file

--- js/myJS.js
+++ js/myJS.js
@@ -1,22 +1,13 @@
-var mySideA=prompt('Enter SIDE A: ');
-var mySideB=prompt('Enter SIDE B: ');
-var mySideC=prompt('Enter SIDE C: ');
 
-function isTriangle(a,b,c){
-	if ( (a+b>c) || (b+c>a) || (a+c>b) ){
+//my js file
+function isTriangle(){
+  var a = parseInt(document.getElementById('mySideA').value);
+  var b = parseInt(document.getElementById('mySideB').value);
+  var c = parseInt(document.getElementById('mySideC').value);
+
+	if ( (a+b>c) && (b+c>a) && (a+c>b) ){
   	alert("This is a triangle");
   }
   else{ alert("This is not a triangle"); }
-}
 
-isTriangle(mySideA,mySideB,mySideC);
-
-if (mySideA==mySideB && mySideA==mySideC && mySideB==mySideC ){
-	alert('Equilateral: All sides are equal!');
-}
-else if(mySideA==mySideB || mySideA==mySideC || mySideB==mySideC ){
-	alert('Isosceles: Exactly 2 sides are equal.');
-}
-else {
-	alert('Scalene: No sides are equal.');
 }
